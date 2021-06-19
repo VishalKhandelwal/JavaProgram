2a76e600cd07b23406fe708100a48bc873f9314d
VishalKhandelwal <vishalshekhar9316@gmail.com>
2021-06-19 15:21:48 +0530

    String Permutation

---
+++ JavaProgram/src/main/java/com/vk/program/StringPermutations.java
@@ -0,0 +1,45 @@
+package com.vk.program;
+/**
+  * Java program to find all permutations of a given String using recursion. 
+  * For example, given a String "XYZ", this program will print all 6 possible permutations of
+  * input e.g. XYZ, XZY, YXZ, YZX, ZXY, XYX
+  *
+  *Write a Java program to print all permutations of a given String.
+  *For example, if given String is "GOD" then your program should 
+  *print all 6 permutations of this string, e.g. "GOD," "OGD," "DOG," "GDO," "ODG," and "DGO."
+  *
+  * @author Javin Paul
+  */
+public class StringPermutations {
+
+    public static void main(String args[]) {
+        permutation("GOD");
+    }
+
+   
+ /*
+  * A method exposed to client to calculate permutation of String in Java. 
+  */
+   public static void permutation(String input){
+          permutation("", input);
+   }
+
+   /*
+    * Recursive method which actually prints all permutations
+    * of given String, but since we are passing an empty String
+    * as current permutation to start with,
+    * I have made this method private and didn't exposed it to client. 
+    */
+   private static void permutation(String perm, String word) {
+        if (word.isEmpty()) {
+            System.out.println(perm + word);
+
+        } else {
+            for (int i = 0; i < word.length(); i++) {
+                permutation(perm + word.charAt(i), word.substring(0, i) 
+                                        + word.substring(i + 1, word.length()));
+            }
+        }
+
+    }
+}
